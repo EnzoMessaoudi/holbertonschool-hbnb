5fc14b1850bef0727a85a0adbabc2fc629c5de45
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-24 15:26:06 +0100

    ADD a basemodel to avoid repetition

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,23 +1,23 @@
 import uuid
 from datetime import datetime
+from basemodel import BaseModel
 
-class place:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+class place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []  # List to store related reviews
+        self.amenities = []  # List to store related amenities
 
-    def save(self):
-        """
-        Update the attribute update_at.
-        """
-        self.updated_at = datetime.now()
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
 
-    def update(self, data):
-        """
-        Use if the user want to update info about something that belongs to him
-        """
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.save
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,23 +1,6 @@
 import uuid
 from datetime import datetime
+from basemodel import BaseModel
 
-class user:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def save(self):
-        """
-        Update the attribute update_at.
-        """
-        self.updated_at = datetime.now()
-
-    def update(self, data):
-        """
-        Use if the user want to update info about something that belongs to him
-        """
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.save
+class user(BaseModel):
+    print("coucou !")
