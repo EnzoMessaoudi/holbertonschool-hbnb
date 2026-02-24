ddc8733c42c321a17a76226489be41bd374355b1
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-24 17:04:30 +0100

    ADD classes attributes and methods for user, place and review (need changes later)

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -3,4 +3,9 @@ from datetime import datetime
 from basemodel import BaseModel
 
 class review(BaseModel):
-    print("coucou !")
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.place = place
+        self.user = user
--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -3,4 +3,18 @@ from datetime import datetime
 from basemodel import BaseModel
 
 class user(BaseModel):
-    print("coucou !")
+    def __init__(self, first_name, last_name, email, password, is_admin, places, reviews):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.is_admin = is_admin
+        self.places = []
+        self.reviews = []
+
+    def add_place(self, place):
+        """
+        Adds a place
+        """
+        self.places.append(place)
