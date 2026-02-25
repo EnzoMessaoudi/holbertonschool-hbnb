093179c76208c98d5a85d33f1de0ab72a7922de5
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-25 13:27:28 +0100

    >ADD email requierements

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 from .basemodel import BaseModel
+import re
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False, places=None):
@@ -15,7 +16,7 @@ class User(BaseModel):
     @property
     def First_name(self):
         """
-        Check if the user name is good
+        Check if the user name is a string, non empty and less than 50 char
         """
         return self.first_name
     
@@ -29,13 +30,12 @@ class User(BaseModel):
 
         if len(value) > 50:
             raise ValueError ("Line over 50 characters")
-        
         self.first_name = value
 
     @property
     def Last_name(self):
         """
-        Check if the user last name is good
+        Check if the user last name is a string, non empty and less than 50 chars
         """
         return self.last_name
     
@@ -49,8 +49,25 @@ class User(BaseModel):
 
         if len(value) > 50:
             raise ValueError ("Line over 50 characters")
-        
         self.last_name = value
+
+    @property
+    def Email(self):
+        return self.email
+    
+    @Email.setter
+    def Email(self, value):
+        """
+        Check if the email is a string, non empty and in the good format. !!!!!! Need to check if it's already used !!!!!!
+        """
+        regex = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}"
+        if not isinstance(value, str):
+            raise TypeError ("Email must be a string")
+        if value == "":
+            raise ValueError ("Email is required")
+        if  not re.fullmatch(regex, value):
+            raise ValueError("Email must be in format: myemail@address.com")
+        self.email = value
 
     def add_place(self, place):
         """
