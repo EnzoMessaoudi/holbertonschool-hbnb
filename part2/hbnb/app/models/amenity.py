aad15a69a478038d5d4232930ced44e2f57b4f24
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-27 17:14:40 +0100

    ADD pycostyle

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -2,6 +2,7 @@ import uuid
 from datetime import datetime
 from .basemodel import BaseModel
 
+
 class Amenity(BaseModel):
     def __init__(self, name, places=None):
         super().__init__()
@@ -12,15 +13,15 @@ class Amenity(BaseModel):
     def name(self):
         """
         Check if the name is a non empty string and is less than 50 chars
-        """    
+        """
         return self._name
-        
+
     @name.setter
     def name(self, value):
-        if not isinstance( value, str):
-            raise TypeError ("Name must be a string")
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
         if value == "":
-            raise ValueError ("Name is required")
+            raise ValueError("Name is required")
         if len(value) > 50:
-            raise ValueError ("Name must be less than 50 character")
+            raise ValueError("Name must be less than 50 character")
         self._name = value
