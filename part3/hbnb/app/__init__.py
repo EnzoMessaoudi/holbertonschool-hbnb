868fd6b4e914ee9734b4d8ac6ec794be23d161bf
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-18 14:45:22 +0100

    ADD the database correct confg

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,9 +7,12 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -17,6 +20,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     bcrypt.init_app(app)
     jwt.init_app(app)
+    db.init_app(app)
 
     # Register the namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
