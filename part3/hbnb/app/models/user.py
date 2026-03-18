1d58b8c28ded9257461994a56778dc9583fb121f
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-18 15:27:03 +0100

    Use SQLalchemy now instead of in memory (user only)

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,96 +1,52 @@
 import uuid
 from datetime import datetime
 from .basemodel import BaseModel
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import db, Bcrypt
+from sqlalchemy.orm import validates
 import re
 
 bcrypt = Bcrypt()
 
 class User(BaseModel):
-    def __init__(self,
-                 first_name,
-                 last_name,
-                 email,
-                 password,
-                 is_admin=False,
-                 places=None
-                 ):
-        super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
-        self.places = []
-        self.reviews = []
-        self.password = password
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
-    @property
-    def first_name(self):
-        """
-        Check if the user name is a string, non empty and less than 50 char
-        """
-        return self._first_name
-
-    @first_name.setter
-    def first_name(self, value):
+    @validates("first_name")
+    def validate_first_name(self, key, value):
         if not isinstance(value, str):
             raise TypeError("First Name must be a string")
-
         if value == "":
-            raise ValueError("First Name requiered")
-
+            raise ValueError("First Name is required")
         if len(value) > 50:
-            raise ValueError("Line over 50 characters")
-        self._first_name = value
+            raise ValueError("First Name must be less than 50 characters")
+        return value
 
-    @property
-    def last_name(self):
-        """
-        Check if the user last name is nom-empty string and less than 50 chars
-        """
-        return self._last_name
-
-    @last_name.setter
-    def last_name(self, value):
+    @validates("last_name")
+    def validate_last_name(self, key, value):
         if not isinstance(value, str):
             raise TypeError("Last Name must be a string")
-
         if value == "":
-            raise ValueError("Last Name requiered")
-
+            raise ValueError("Last Name is required")
         if len(value) > 50:
-            raise ValueError("Line over 50 characters")
-        self._last_name = value
+            raise ValueError("Last Name must be less than 50 characters")
+        return value
 
-    @property
-    def email(self):
-        return self._email
-
-    @email.setter
-    def email(self, value):
-        """
-        Check if the email is a string, non empty and in the good format.
-        """
+    @validates("email")
+    def validate_email(self, key, value):
         regex = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}"
-
         if not isinstance(value, str):
             raise TypeError("Email must be a string")
         if value == "":
             raise ValueError("Email is required")
         if not re.fullmatch(regex, value):
             raise ValueError("Email must be in format: myemail@address.com")
-        self._email = value
-
-    def add_place(self, place):
-        """
-        Adds a place
-        """
-        self.places.append(place)
+        return value
 
     def hash_password(self, password):
-        """Hashes the password before storing it."""
-        return bcrypt.generate_password_hash(password).decode('utf-8')
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def verify_password(self, password):
-        """Verifies if the provided password matches the hashed password."""
         return bcrypt.check_password_hash(self.password, password)
