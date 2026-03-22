f47e60e405f583d94acc3905a1cb1137a7c612c2
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-22 10:53:52 +0100

    ADD db.Model to BaseModel to inherit it

--- part3/hbnb/app/models/basemodel.py
+++ part3/hbnb/app/models/basemodel.py
@@ -2,58 +2,32 @@ from app import db
 import uuid
 from datetime import datetime
 
-
-class BaseModel:
-    __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
+class BaseModel(db.Model):
+    __abstract__ = True
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def save(self):
-        """Update the updated_at timestamp whenever the object is modified"""
+        """Met à jour updated_at et commit si nécessaire"""
         self.updated_at = datetime.now()
+        db.session.add(self)
+        db.session.commit()
 
     def update(self, data):
-        """Update the attributes of the object based on his dictionary"""
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.save()
-    
-    import uuid
-from datetime import datetime
-
-
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def save(self):
-        """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now()
-
-    def update(self, data):
-        """Update the attributes of the object based on his dictionary"""
+        """Met à jour les attributs depuis un dictionnaire"""
         for key, value in data.items():
             if hasattr(self, key):
                 setattr(self, key, value)
         self.save()
 
     def to_dict(self):
-        created = None
-        updated = None
-
-        if self.created_at:
-            created = self.created_at.isoformat()
-
-        if self.updated_at:
-            updated = self.updated_at.isoformat()
-
+        created = self.created_at.isoformat() if self.created_at else None
+        updated = self.updated_at.isoformat() if self.updated_at else None
         return {
             "id": self.id,
             "created_at": created,
             "updated_at": updated
         }
+    
