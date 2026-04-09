01b2a6df92481e71411186553c55b9ed35766b5e
EnzoMessaoudi <12168@holbertonstudents.com>
2026-04-09 14:54:01 +0200

    ADD place details

--- part4/hbnb/base_files/scripts.js
+++ part4/hbnb/base_files/scripts.js
@@ -2,6 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
     setupLogin();
     checkAuthentication();
     setupPriceFilter();
+
+    // Detect if we are on place.html and load details
+    const placeDetailsContainer = document.getElementById("place-details");
+    if (placeDetailsContainer) {
+        const placeId = getPlaceIdFromURL();
+        const token = getCookie('token'); // can be null if not logged in
+        if (placeId) {
+            fetchPlaceDetails(token, placeId);
+        }
+    }
 });
 
 let allPlaces = [];
@@ -62,7 +72,7 @@ async function fetchPlaces(token) {
     }
 }
 
-// Function that check if a user is connected or no
+// Function that check if a user is connected or no (Used in Index and place details)
 function checkAuthentication() {
     const token = getCookie('token');
     const loginLink = document.getElementById('login-link');
@@ -124,9 +134,9 @@ function setupPriceFilter() {
 
     priceFilter.innerHTML = `
         <option value="">All</option>
-        <option value="50">$10</option>
-        <option value="100">$50</option>
-        <option value="200">$100</option>
+        <option value="10">$10</option>
+        <option value="50">$50</option>
+        <option value="100">$100</option>
     `;
 
     priceFilter.addEventListener("change", () => {
@@ -136,4 +146,83 @@ function setupPriceFilter() {
             : allPlaces;
         displayPlaces(filteredPlaces);
     });
+}
+
+//Get the ID of the place from the URL
+function getPlaceIdFromURL() {
+    let params = new URLSearchParams(location.search);
+    const id = params.get('id');
+    return id;
+}
+
+async function fetchPlaceDetails(token, placeId) {
+    try {
+        const response = await fetch(`http://127.0.0.1:5000/api/v1/places/${placeId}`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json();
+
+        if (!data) {
+            placeDetails.innerHTML = "<p>Place not found.</p>";
+            return;
+}
+
+        displayPlaceDetails(data);
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+function displayPlaceDetails(place) {
+    // Clear the current content of the place details section
+    const placeDetails = document.getElementById("place-details");
+    if (!place || !placeDetails) {
+    if (placeDetails) placeDetails.innerHTML = "<p>Place not found.</p>";
+    return;
+}
+
+    if (!placeDetails) return;
+
+    // Create elements to display the place details (name, description, price, amenities and reviews)
+
+    let amenities = "None";
+    if (Array.isArray(place.amenities) && place.amenities.length > 0) {
+        amenities = place.amenities.map(a => a.name).join(', ');
+    }
+
+    let reviewsHTML = "No reviews yet.";
+    if (place.reviews && place.reviews.length > 0) {
+        reviewsHTML = place.reviews.map(r => `<p><strong>${r.user}:</strong> ${r.text}</p>`).join('');
+    }
+
+    placeDetails.innerHTML = `
+        <div class ="place-details">
+            <h2>${place.title}</h2>
+            <p>${place.description}</p>
+            <p>Price per night: $${place.price}</p>
+            <p>Amenities: ${amenities}</p>
+            <p>reviews: ${reviewsHTML}</p>
+        </div>
+    `;
+}
+
+function checkAuthentication() {
+    const token = getCookie('token');
+    const loginLink = document.getElementById('login-link');
+    const addReviewLink = document.getElementById('add-review');
+
+    if (!token) {
+        if (loginLink)
+        loginLink.style.display = 'block';
+        fetchPlaces()
+        addReviewLink.style.display = 'none';
+    } else {
+        if (loginLink) loginLink.style.display = 'none';
+        fetchPlaces(token);
+    }
 }
